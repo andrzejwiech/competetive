e11cd7547139c2bb1376132e3723783da6c173ad
andrzejwiech <awiech@me.com>
2018-10-19 16:49:19 +0200

    number of zeros

--- src/com/wiech/algorithms/recursion/easy/NumberOfZeros.java
+++ src/com/wiech/algorithms/recursion/easy/NumberOfZeros.java
@@ -1,10 +1,14 @@
 package com.wiech.algorithms.recursion.easy;
 
+import static java.lang.Math.abs;
+
 public class NumberOfZeros {
 
     public int count(int n) {
         if (n == 0)
-            return n;
+            return 1;
+        else if (abs(n) <= 9)
+            return 0;
         else
             return (n % 10 == 0 ? 1 : 0) + count(n / 10);
     }
