56cc3ade8c5ecea1bf0340361449a2c87302556f
andrzejwiech <awiech@me.com>
2018-10-23 07:52:46 +0200

    list nlogn

--- src/com/wiech/algorithms/dynamicprogramming/easy/LongestIncreasingSubsequence.java
+++ src/com/wiech/algorithms/dynamicprogramming/easy/LongestIncreasingSubsequence.java
@@ -3,11 +3,11 @@ package com.wiech.algorithms.dynamicprogramming.easy;
 import static java.lang.Math.max;
 import java.util.Arrays;
 
-/**
- * O(n^2)
- */
 public class LongestIncreasingSubsequence {
 
+    /**
+     * O(n^2)
+     */
     public int longest(int[] arr) {
 
         int l = arr.length;
@@ -21,5 +21,39 @@ public class LongestIncreasingSubsequence {
         }
         return state[l - 1];
 
+    }
+
+    /**
+     * O(nlogn)
+     */
+    public int longest2(int[] arr) {
+
+        int[] result = new int[arr.length];
+        int len = 1;
+        result[0] = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < result[0])
+                result[0] = arr[0];
+            else if (arr[i] > result[len - 1])
+                result[len++] = arr[i];
+            else
+                result[replace(result, len - 1, arr[i])] = arr[i];
+
+        }
+        return len;
+    }
+
+    private int replace(int[] arr, int end, int element) {
+        int start = 0;
+
+        while (start != end) {
+            int middle = start + (end - start) / 2;
+            if (arr[middle] > element)
+                end = middle;
+            else
+                start = middle + 1;
+        }
+        return end;
     }
 }
