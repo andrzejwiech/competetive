df2ec911fc4a6b1679145765735ac7d6bd3183c6
andrzejwiech <awiech@me.com>
2019-02-01 22:55:29 +0100

    no commit: sum

---
+++ src/com/wiech/algorithms/general/medium/easy/AddUp.java
@@ -0,0 +1,22 @@
+package com.wiech.algorithms.general.medium.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class AddUp {
+    /**
+     * @author Andrzej Wiech
+     */
+    boolean hasPairWitSum(int values[], int sum) {
+
+        Set<Integer> compounds = new HashSet<>();
+
+        for (int value : values) {
+            if (compounds.contains(value)) {
+                return true;
+            }
+            compounds.add(sum - value);
+        }
+        return false;
+    }
+}
