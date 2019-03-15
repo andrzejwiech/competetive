2764dafb017f35f7afd168cce40921a4a69443c6
andrzejwiech <awiech@me.com>
2019-03-15 18:18:47 +0100

    trees warm up

--- src/com/wiech/algorithms/trees/BinaryTree.java
+++ src/com/wiech/algorithms/trees/BinaryTree.java
@@ -1,5 +1,8 @@
 package com.wiech.algorithms.trees;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import static java.lang.Math.max;
 
 class BinaryTree {
@@ -10,15 +13,48 @@ class BinaryTree {
         reverse(root);
     }
 
+    private Node reverse(Node node) {
+        if (node == null)
+            return null;
+        Node tmp = node.left;
+        node.left = node.right;
+        node.right = tmp;
+        node.right = reverse(node.right);
+        node.left = reverse(node.left);
+
+        return node;
+    }
+
     boolean isSymmetric() {
         if (root == null) {
             return true;
         }
         return isSymmetric(root.left, root.right);
     }
+
+    private boolean isSymmetric(Node left, Node right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+
     boolean isTheSame(Node node) {
 
         return isTheSame(root, node);
+    }
+
+    private boolean isTheSame(Node one, Node two) {
+        if (one == null && two == null)
+            return true;
+        else if (one == null || two == null)
+            return false;
+        else if (one.value != two.value)
+            return false;
+        return isTheSame(one.left, two.left) && isTheSame(one.right, two.right);
     }
 
     int height() {
@@ -34,39 +70,18 @@ class BinaryTree {
         return max(heightLeft, heightRight);
     }
 
-    private boolean isSymmetric(Node left, Node right) {
-        if (left == null && right == null) {
-            return true;
+    void breadthFirstTraversal() {
+        if (root == null) {
+            return;
         }
-        if (left == null || right == null) {
-            return false;
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            System.out.println(current.value);
+            if (current.left != null) queue.add(current.left);
+            if (current.right != null) queue.add(current.right);
         }
-        return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
     }
-
-    private Node reverse(Node node) {
-        if (node == null)
-            return null;
-
-        Node tmp = node.left;
-        node.left = node.right;
-        node.right = tmp;
-        node.right = reverse(node.right);
-        node.left = reverse(node.left);
-
-        return node;
-    }
-
-
-    private boolean isTheSame(Node one, Node two) {
-
-        if (one == null && two == null)
-            return true;
-        else if (one == null || two == null)
-            return false;
-        else if (one.value != two.value)
-            return false;
-        return isTheSame(one.left, two.left) && isTheSame(one.right, two.right);
-    }
-
 }
