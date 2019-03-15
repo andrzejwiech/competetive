fe72d75695c49e323e5c4b1fbfd192710a2070c6
andrzejwiech <awiech@me.com>
2019-03-15 07:42:07 +0100

    trees warm up

--- src/com/wiech/algorithms/trees/BinaryTree.java
+++ src/com/wiech/algorithms/trees/BinaryTree.java
@@ -1,5 +1,7 @@
 package com.wiech.algorithms.trees;
 
+import static java.lang.Math.max;
+
 class BinaryTree {
 
     Node root;
@@ -28,11 +30,43 @@ class BinaryTree {
     private Node reverse(Node node) {
         if (node == null)
             return null;
+
         Node tmp = node.left;
         node.left = node.right;
         node.right = tmp;
         node.right = reverse(node.right);
         node.left = reverse(node.left);
+
         return node;
     }
+
+    boolean isTheSame(Node node) {
+
+        return isTheSame(root, node);
+    }
+
+    int height() {
+        return height(root);
+    }
+
+    private int height(Node root) {
+        if (root == null)
+            return 0;
+        int heightLeft = height(root.left) + 1;
+        int heightRight = height(root.right) + 1;
+
+        return max(heightLeft, heightRight);
+    }
+
+    private boolean isTheSame(Node one, Node two) {
+
+        if (one == null && two == null)
+            return true;
+        else if (one == null || two == null)
+            return false;
+        else if (one.value != two.value)
+            return false;
+        return isTheSame(one.left, two.left) && isTheSame(one.right, two.right);
+    }
+
 }
