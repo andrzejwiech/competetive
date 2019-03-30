fc1bf5590df4478487bd415265c4c02b2cf9b81b
andrzejwiech <awiech@me.com>
2019-03-30 20:13:22 +0100

    lis

---
+++ src/com/wiech/algorithms/recursion/easy/LongestIncreasingSequence.java
@@ -0,0 +1,18 @@
+package com.wiech.algorithms.recursion.easy;
+
+public class LongestIncreasingSequence {
+
+    public int max(int[] sequence) {
+        return max(sequence, 0, Integer.MIN_VALUE);
+    }
+
+    private int max(int[] sequence, int n, int last) {
+        if (n == sequence.length) {
+            return 0;
+        }
+        if (sequence[n] <= last) {˚
+            return max(sequence, n + 1, last);
+        }
+        return Math.max(max(sequence, n + 1, last), 1 + max(sequence, n + 1, sequence[n]));
+    }
+}
